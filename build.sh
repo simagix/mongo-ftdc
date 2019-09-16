d94275c57532feeaf2bc9b801d83fa3e8902ba59
Kuei-chun Chen <simagix@gmail.com>
2019-09-16 08:35:33 -0400

    update templates

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 # Copyright 2019 Kuei-chun Chen. All rights reserved.
-docker-compose down
 
 # build simagix/ftdc
 dep ensure -update
-ver=0.3.0
-env GOOS=linux GOARCH=amd64 go build -o ftdc-linux-x64 simple_json.go
-docker build -f Dockerfile -t simagix/ftdc -t simagix/ftdc:${ver} .
-rm -f ftdc-linux-x64
-mkdir -p ./diagnostic.data/
+ver=0.3.1
+mkdir -p bin
+env GOOS=darwin GOARCH=amd64 go build -o bin/ftdc-osx-x64 simple_json.go
 
 # build simagix/grafana-ftdc
-docker build -f grafana/Dockerfile -t simagix/grafana-ftdc -t simagix/grafana-ftdc:${ver} .
-
-docker rmi -f $(docker images -f "dangling=true" -q)
+if [ "$1" == "docker" ]; then
+    env GOOS=linux GOARCH=amd64 go build -o ftdc-linux-x64 simple_json.go
+    docker build -f Dockerfile -t simagix/ftdc -t simagix/ftdc:${ver} .
+    rm -f ftdc-linux-x64
+    mkdir -p ./diagnostic.data/
+    docker-compose down
+    docker build -f grafana/Dockerfile -t simagix/grafana-ftdc -t simagix/grafana-ftdc:${ver} .
+    docker rmi -f $(docker images -f "dangling=true" -q)
+fi
